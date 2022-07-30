901e42fc1816b79a9d738b3fe030d61e01d138ec
oradbarel <75837421+oradbarel@users.noreply.github.com>
2022-07-30 18:07:58 +0300

    adding new file: Algorithms.py

--- src/Gragh.py
+++ src/Gragh.py
@@ -2,30 +2,102 @@
 
 ##### Constants:
 
-QUIT_CHAR = 'q'
-YES_CHAR = 'y'
-NOT_CHAR = 'n'
 
-##### Classes:
+DEFAULT_WEIGHT = 1
+DEFAULT_COLOR = None
 
-class QuitError(Exception):
-    pass
+#####=====================================
+
+
+class Vertex:
+    """
+    Class for representing a vertex in graph, for future algorithms.
+    """
+    def __init__(self, key, color = None) -> None:
+        self._key = key
+        self._color = color
+    
+    def getKey(self):
+        return self._key
+
+    def getColor(self):
+        return self._color
+
+    def setColor(self, color) -> None:
+        self._color = color
+
+class Edge:
+    """
+    Base class for representing an egde.
+    You should one of its subclasses - `DirectedEdge` or `UndirectedEdge`.
+    """
+    def __init__(self, src: int, dst: int, weight: float = DEFAULT_WEIGHT, color = DEFAULT_COLOR) -> None:
+        self._src = src
+        self._dst = dst
+        self._weight = weight
+        self._color = color
+
+    def __repr__(self) -> str:
+        return str(self)
+    
+    def __str__(self) -> str:
+        return '({0}, {1}). weight = {2}. color = {3}'.format(self._src, self._dst, self._weight, self._color)
+
+    def getSource(self) -> int:
+        return self._src
+
+    def getDestination(self) -> int:
+        return self._dst
+
+    def getEdges(self) -> tuple[int, int]:
+        return (self._src, self._dst)
+
+    def getWeight(self):
+        return self._weight
+
+    def setWeight(self, weight) -> None:
+        self._weight = weight
+
+    def getColor(self):
+        return self._color
+
+    def setColor(self, color) -> None:
+        self._color = color
+
+class DirectedEdge(Edge):
+    def __init__(self, src: int, dst: int, weight: float = DEFAULT_WEIGHT, color=DEFAULT_COLOR) -> None:
+        super().__init__(src, dst, weight, color)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+    def getEdges(self) -> tuple[int, int]:
+        return super().getEdges()
+
+class UndirectedEdge(Edge):
+    def __init__(self, src: int, dst: int, weight: float = DEFAULT_WEIGHT, color=DEFAULT_COLOR) -> None:
+        super().__init__(src, dst, weight, color)
+
+    def __str__(self) -> str:
+        return super().__str__().replace("(", "{").replace(")", "}")
+
+    def getEdges(self) -> set[int]:
+        return set((super().getEdges()))
 
 class Graph:
     """
-    Abstract Base class for representing a graph - either directed or undirected.
+    Base class for representing a graph - either directed or undirected.
+    You should one of its subclasses - `DirectedGraph` or `UndirectedGraph`.
     """
-    def __init__(self, V: list, E: list) -> None:
+    def __init__(self, V: list, E: list[Edge] = []) -> None:
         self._V = V
         self._E = E
-        self._verts_num = len(self._V)
-        if(self._verts_num == 0):
+        if(len(self._V) == 0):
             raise ValueError
-        self._edges_num = len(self._E)
         self._adjacency_list = self.__makeAdjacencyList__()
 
-    def __makeAdjacencyList__(self) -> list:
-            return {i: [] for i in range(1, self._verts_num+1)}
+    def __makeAdjacencyList__(self) -> dict:
+            return {i: [] for i in range(1, len(self._V)+1)}
     
     def getVertices(self):
         return self._V
@@ -48,16 +120,29 @@ class Graph:
         except:
             raise TypeError("argument must be an integer")            
         if(v not in self._V):
-            raise ValueError("argument must be an integer between 1 to {0}".format(self._verts_num))
+            raise ValueError("argument must be an integer between 1 to {0}".format(len(self._V)))
         return self._adjacency_list[v]
     
+    def addVertex(self, v: int) -> None:
+        """
+        Function for algorithms uses (not for user).
+        """
+        try:
+            v = int(v)
+        except:
+            raise TypeError("argument must be an integer")            
+        if(v in self._V):
+            raise ValueError("vertex {0} already exists".format(len(self._V)))
+        self._V.append(v)
+        self._adjacency_list[v] = []
+
     def isEdgeExists(self, src: int, dst: int):
         """
         Checks if the edge (src, dst) exists in the graph or not.
         Complexity: O(d_out(src)).
-        :param src: The source vertice.
+        :param src: The source vertex.
         :type src: int.
-        :param dst: The destination vertice.
+        :param dst: The destination vertex.
         :type dst: int.
         :returns: True - if exists, False if does not exist
         :rtype: bool
@@ -69,7 +154,7 @@ class Graph:
         except:
             raise TypeError("arguments must be an integer")            
         if((src not in self._V) or (dst not in self._V)):
-            raise ValueError("arguments must be an integer between 1 to {0}".format(self._verts_num))
+            raise ValueError("arguments must be an integer between 1 to {0}".format(len(self._V)))
         src_neighbors = self.getNeighborsOf(src)
         return (dst in src_neighbors)
 
@@ -78,113 +163,26 @@ class DirectedGraph(Graph):
     def __init__(self, V: list, E: list) -> None:
         super().__init__(V, E)
 
-    def __makeAdjacencyList__(self) -> list:
+    def __makeAdjacencyList__(self) -> dict:
         adjacency_list = super().__makeAdjacencyList__()
-        for (src,dst) in self._E:
-            adjacency_list[src].append(dst)
+        for edge in self._E:
+            src = edge.getSource()
+            adjacency_list[src].append(edge)
         return adjacency_list
-
 
 class UndirectedGraph(Graph):
 
     def __init__(self, V: list, E: list) -> None:
         super().__init__(V, E)
 
-    def __makeAdjacencyList__(self) -> list:
+    def __makeAdjacencyList__(self) -> dict:
         adjacency_list = super().__makeAdjacencyList__()
-        for (src,dst) in self._E:
-            adjacency_list[src].append(dst)
-            adjacency_list[dst].append(src)
+        for edge in self._E:
+            (v1, v2) = tuple(edge.getEdges())
+            adjacency_list[v1].append(edge)
+            adjacency_list[v2].append(edge)
         return adjacency_list
 
-
-##### Functions:
-
-def getIfDirectedFromTerminal() -> bool:
-    directed = ''
-    while(1):
-        directed = input("Press 'y' for directed graph or 'n' for undirected graph: ").lower()
-        if (directed in [YES_CHAR, NOT_CHAR]):
-            break
-    return (directed == YES_CHAR)
-
-def getEdgeFromTerminal(verts_list: list) -> tuple:
-    """Gets an edge from user by the keybord. Returns it as a tuple.
-
-:param verts_list: A list of the vertices.
-:type verts_list: list
-:returns: The edge - (src, dst)
-:rtype: tuple
-"""
-    n = len(verts_list)
-    while(1):
-        src = input("Enter source vertice: ")
-        if(src == QUIT_CHAR):
-            raise QuitError
-        try:
-            src = int(src)
-            if(src in verts_list):
-                break
-        except:
-            pass
-        print("Please insert an integer between 1 to {0}".format(n))
-
-    while(1):
-        dst = input("Enter destination vertice: ")
-        if(dst == QUIT_CHAR):
-            raise QuitError
-        try:
-            dst = int(dst)
-            if(dst in verts_list):
-                break
-        except:
-            pass
-        print("Please insert an integer between 1 to {0}".format(n))
-    return (src, dst)
-
-def getGraphFromTerminal() -> tuple:
-    
-    directed = getIfDirectedFromTerminal()
-
-    n = int(input("Insert number of vertices: "))
-    V = [i for i in range(1, n+1)]
-    print("V is: " + str(V))
-
-    E = []
-    print("Now, enter the edges (you may prees 'q' when you want to finish):")
-    while(1):
-        try:
-            (src, dst) = getEdgeFromTerminal(V)
-        except(QuitError):
-            break
-        e = ((int(src), int(dst)))
-        E.append(e)
-        print("edge #{0}: {1}.".format(len(E), e))
-    print("E is: " + str(E))
-
-    return (V, E, directed)
-
-def getGraphFromJson():
-    pass
-
-def getGraphFromGUI():
-    pass
-
-##### Main Code:
-
-def main():
-    """
-    (V, E, directed) = getGraphFromTerminal()
-    my_graph = DirectedGraph(V, E) if directed else UndirectedGraph(V, E)
-    # """
-
-    my_gr1 = DirectedGraph([1, 2, 3, 4, 5], [(1,2), (5,2), (4,3)])
-    print(my_gr1._adjacency_list)
-    my_gr2 = UndirectedGraph([1, 2, 3, 4, 5], [(1,2), (5,2), (4,3)])
-    print(my_gr2._adjacency_list)
-    print("neighbors of 2: " + str(my_gr2.getNeighborsOf(2)))
+#####=====================================
 
 
-
-if __name__ == "__main__":
-    main()
